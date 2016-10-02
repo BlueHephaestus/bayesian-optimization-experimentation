2fb2d97cfed7a5ba0a28f6a3ea6fcdef66a695df
DarkElement75 <blakeedwards@gmail.com>
2016-10-02 13:48:44 -0600

    Modularized and functional with n dimensions

--- bayesian_optimization1.py
+++ bayesian_optimization1.py
@@ -31,99 +31,87 @@ Given an input function,
 import sys
 
 import numpy as np
+from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 import hyperparameter
 from hyperparameter import HyperParameter
 
+import acquisition_functions
+from acquisition_functions import *
+
+import covariance_functions
+from covariance_functions import *
+
+import gaussian_base
+from gaussian_base import *
+
 #Our chosen test black box optimization function
+"""
 def bbf(x):
    #return np.sin(x**(2.5/2.0))+(x/3.6)**2
-   return np.exp(-(x-2)**2) + np.exp(-((x-6)**2)/10.0) + (1.0/(x**2 + 1))
    #return (x-7)**3 -3.5*(x-7)**2 + 4
+"""
 
-def gaussian_distribution(x, mean, variance):
-    #Gets input x, mean, and variance
-    #Returns vector or scalar from input
-    return (np.exp(-((x-mean)**2)/(2*variance))) / (np.sqrt(2*variance*np.pi))
-
-def cdf(x, mean, variance):
-    #Get values to compute cdf over
-    dist_values = gaussian_distribution(np.arange(x-100, x, .1), mean, variance)
-    
-    #Equivalent to the last element of cumulative sum
-    return sum(dist_values)
-
-def cov(x_i, x_j):
-    return np.exp(-.95*np.linalg.norm(x_i - x_j)**2) 
-
-def get_cov_matrix(f):
-    #Given a vector f, generate the covariance matrix 
-    #f because known inputs
-    f_n = len(f)
-    f_m = np.zeros(shape=(f_n, f_n))
-    for row_i, f_i in enumerate(f):
-        for col_i, f_j in enumerate(f):
-            f_m[row_i, col_i] = cov(f_i, f_j)
-
-    return f_m
-
-def get_cov_vector(f, test_f):
-    #Given a vector f and scalar f* (test_f)
-    #Generate a covariance vector for each value in f
-    f_n = len(f)
-    f_v = np.zeros(shape=(f_n, 1))
-    for row_i, f_i in enumerate(f):
-        f_v[row_i] = cov(test_f, f_i)
-
-    return f_v
+def bbf(x):
+    #return -(x-2)**2
+    return np.exp(-(x-2)**2) + np.exp(-((x-6)**2)/10.0) + (1.0/(x**2 + 1))
+    #return np.cos(x[0])*np.cos(x[1])*np.exp(-((x[0]-np.pi)**2 + (x[1]-np.pi)**2))
+    #return -(x[0]-2)**2 - (x[1]-3)**2 - (x[2]-1)**2+ 4
         
-class upper_confidence_bound(object):
-    """CURRENTLY BROKEN AFAIK"""
-    
-    @staticmethod
-    def evaluate(means, variances, values, confidence_interval):
-        return np.argmax(means + confidence_interval * variances)
-
-class probability_improvement(object):
-    
-    @staticmethod
-    def evaluate(means, variances, values, confidence_interval):
-        improvement_probs = [np.nan_to_num(cdf(val, mean, variance)) for val, mean, variance in zip(values, means, variances)]
-        return np.argmax(improvement_probs)
-
 #Number of evaluated input points / level of detail
-n = 1000
+detail_n = 30
 
 #If we want the highest point or lowest point
 maximizing = True
 
 #Number of bbf evaluations allowed to perform before ending optimization
-bbf_evaluation_n = 10
+bbf_evaluation_n = 20
 
 #Choice of acquisition function
-#acquisition_function = upper_confidence_bound()
-acquisition_function = probability_improvement()
+acquisition_function = upper_confidence_bound()
+#acquisition_function = expected_improvement()
+#acquisition_function = probability_improvement()
+
+#Choice of covariance function
+covariance_function = squared_exponential()
 
 #Acquisition function hyper parameter choice
-confidence_interval = .95
+confidence_interval = 1.5 
 
 #Initialize ranges for each parameter into a resulting matrix
 hps = [HyperParameter(0, 10)]
 
+#UI settings
+plot_results = True
+plot_3d_results = False
+
 """END TUNABLE PARAMETERS"""
 
-#Initialize multivariate(if len(hps) > 1) input domain
-#We transpose it so we can loop through our dimensional inputs as you would since they are vectors
-domain = np.array([np.arange(hp.min, hp.max, ((hp.max-hp.min)/float(n))) for hp in hps])
-domain_T = domain.transpose()
+#Initialize independent domains of each parameter
+#We will do the cartesian product on the vectors contained here to get our entire sets of multidimensional inputs
+independent_domains = np.array([np.arange(hp.min, hp.max, ((hp.max-hp.min)/float(detail_n))) for hp in hps])
 
-#Temporary domain so we can make sure to get two DIFFERENT values 
-shuffled_domain = np.copy(domain.transpose())
-np.random.shuffle(shuffled_domain)
+#Get the total number of outputs as n^r
+n = detail_n**len(hps)
 
-#Get our different values
-x1, x2 = shuffled_domain[:2]
+#Get the cartesian product of all vectors contained to get entire multidimensional domain
+domain = [np.array(i) for i in cartesian_product(independent_domains)]
+
+#Get our axis vectors if plotting
+if plot_results:
+    domain_x = np.copy(independent_domains[0])
+elif plot_3d_results:
+    domain_x = np.copy(independent_domains[0])
+    domain_y = np.copy(independent_domains[1])
+
+#Now that we have full domain, we can shuffle the original to get two random input vectors
+for independent_domain in independent_domains:
+    np.random.shuffle(independent_domain)
+
+#Get our different values easily by transposing
+x1, x2 = independent_domains.transpose()[:2]
 
 #Now that we have our two random input vectors, evaluate them and store them in our bbf inputs and outputs vector
 #Modify the bbf function when you make this more complicated with input to a bot
@@ -136,19 +124,25 @@ bbf_evaluations = np.array([bbf(x1), bbf(x2)])
 #Our main loop to go through every time we evaluate a new point, until we have exhausted our allowed 
 #   black box function evaluations.
 for bbf_evaluation_i in range(2, bbf_evaluation_n):
+    #sys.stdout.write("\rDetermining Point #%i" % (bbf_evaluation_i+1))
+    #sys.stdout.flush()
+    print "Determining Point #%i" % (bbf_evaluation_i+1)
 
     #Since we reset this every time we generate through the domain
     test_means = np.zeros(shape=(n))
     test_variances = np.zeros(shape=(n))
     test_values = np.zeros(shape=(n))
 
-    for test_input_i, test_input in enumerate(domain_T):
+    for test_input_i, test_input in enumerate(domain):
         #Generate our covariance matrices and vectors
-        training_cov_m = get_cov_matrix(bbf_inputs)#K
+        training_cov_m = get_cov_matrix(bbf_inputs, covariance_function)#K
+        
+        #Clip a small amount so we don't have singular matrix
+        training_cov_m = training_cov_m + (np.eye(training_cov_m.shape[0])*1e-7)
         training_cov_m_inv = np.linalg.inv(training_cov_m)#K^-1
-        test_cov = get_cov_vector(bbf_inputs, test_input)#K*
+        test_cov = get_cov_vector(bbf_inputs, test_input, covariance_function)#K*
         test_cov_T = test_cov.transpose()#K*T
-        test_cov_diag = cov(test_input, test_input)#K**
+        test_cov_diag = covariance_function.evaluate(test_input, test_input)#K**
 
         #Compute test mean using our Multivariate Gaussian Theorems
         #print test_cov_T.shape, training_cov_m_inv.shape, bbf_evaluations.shape
@@ -159,7 +153,7 @@ for bbf_evaluation_i in range(2, bbf_evaluation_n):
 
         #Store them for use with our acquisition function
         test_means[test_input_i] = test_mean
-        test_variances[test_input_i] = test_variance
+        test_variances[test_input_i] = test_variance + 0.01
 
     #Now that we have all our means u* and variances c* for every point in the domain,
     #Move on to determining next point to evaluate using our acquisition function
@@ -169,22 +163,55 @@ for bbf_evaluation_i in range(2, bbf_evaluation_n):
     else:
         test_values = test_means - test_variances
 
+    #Set the filename
+    fname = "results/%s" % str(bbf_evaluation_i)
+
     #Plot our updates
-    """
-    plt.plot(domain[0], test_means)
-    plt.plot(bbf_inputs, bbf_evaluations, 'bo')
-    plt.plot(domain[0], test_means+test_variances, 'r')
-    plt.plot(domain[0], test_means-test_variances, 'r')
-    plt.plot(domain[0], bbf(domain[0]), 'y')
-    plt.show()
-    """
+    #Make these in seperate file once we have 3d working
+    if plot_results:
+        plt.plot(domain_x, test_means)
+        plt.plot(bbf_inputs, bbf_evaluations, 'bo')
+        plt.plot(domain_x, test_means+test_variances, 'r')
+        plt.plot(domain_x, test_means-test_variances, 'r')
+        plt.plot(domain_x, bbf(domain_x), 'y')
+        plt.savefig("%s.jpg" % fname, dpi=None, facecolor='w', edgecolor='w',
+            orientation='portrait', papertype=None, format=None,
+            transparent=False, bbox_inches='tight', pad_inches=0.1,
+            frameon=None)
+        plt.gcf().clear()
+    elif plot_3d_results:
+        if bbf_evaluation_i == bbf_evaluation_n-1:
+            fig = plt.figure()
+            ax = fig.add_subplot(111, projection='3d')
+            #X & Y have to be matrices of all vertices
+            #Z has to be matrix of outputs
+            #Convert our vectors to compatible matrix counterparts
+            Y = np.array([[i] for i in domain_y])
+
+            X = np.tile(domain_x, (detail_n, 1))
+            Y = np.tile(Y, (1, detail_n))
+
+            #This ones easy, just reshape
+            #print X.shape, Y.shape, test_means.shape
+            Z1 = test_means.reshape(detail_n, detail_n)
+            Z2 = test_variances.reshape(detail_n, detail_n)
+            Z3 = (test_means + test_variances).reshape(detail_n, detail_n)
+            Z4 = (test_means - test_variances).reshape(detail_n, detail_n)
+
+
+            ax.plot_surface(X, Y, Z1, rstride=1, cstride=1, cmap=cm.coolwarm)
+            #ax.plot_wireframe(X, Y, Z2, rstride=1, cstride=1)
+            ax.plot_wireframe(X, Y, Z3, rstride=1, cstride=1)
+            ax.plot_wireframe(X, Y, Z4, rstride=1, cstride=1)
+            #plt.show()
 
     #Get the index of the next input to evaluate in our black box function
     #Since acquisition functions return argmax values
     next_input_i = acquisition_function.evaluate(test_means, test_variances, test_values, confidence_interval)
 
     #Add our new input
-    next_input = domain_T[next_input_i]
+    next_input = domain[next_input_i]
+    #print "\tNew point: {}".format(next_input)
     bbf_inputs.append(np.array(next_input))
 
     #Evaluate new input
@@ -196,4 +223,5 @@ for bbf_evaluation_i in range(2, bbf_evaluation_n):
     bbf_evaluations = np.array(bbf_evaluations)
 
 best_input = bbf_inputs[np.argmax(bbf_evaluations)]
-print "Best input found after {} iterations: {}".format(bbf_evaluation_n, best_input[0])
+print bbf_inputs, bbf_evaluations
+print "Best input found after {} iterations: {}".format(bbf_evaluation_n, best_input)
