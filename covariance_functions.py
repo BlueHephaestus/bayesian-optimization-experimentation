cd7948d2d9ea07c131e0090eeda0dc1bfb56548e
DarkElement75 <blakeedwards@gmail.com>
2016-10-02 19:14:40 -0600

    Modularized more and added multiple other covariance functions

--- covariance_functions.py
+++ covariance_functions.py
@@ -1,7 +1,58 @@
 import numpy as np
+import scipy.special as ss
 
-class squared_exponential(object):
+class covariance_function(object):
+    #Superclass
 
-    @staticmethod
-    def evaluate(x_i, x_j):
-        return np.exp(-.5*np.linalg.norm(x_i - x_j)**2) 
+    def __init__(self, lengthscale, v):
+        self.lengthscale = lengthscale
+        self.v = v
+
+class dot_product(covariance_function):
+
+    def __init__(self, lengthscale, v):
+        covariance_function.__init__(self, lengthscale, v)
+
+    def evaluate(self, x_i, x_j):
+        return 1*np.dot(x_i.transpose(), x_j)
+
+class brownian_motion(covariance_function):
+
+    def __init__(self, lengthscale, v):
+        covariance_function.__init__(self, lengthscale, v)
+
+    def evaluate(self, x_i, x_j):
+        return 1*np.min(x_i, x_j)
+
+class squared_exponential(covariance_function):
+
+    def __init__(self, lengthscale, v):
+        covariance_function.__init__(self, lengthscale, v)
+
+    def evaluate(self, x_i, x_j):
+        return np.exp((-1/(2.0*self.lengthscale))*np.linalg.norm(x_i - x_j)**2) 
+
+class ornstein_uhlenbeck(covariance_function):
+
+    def __init__(self, lengthscale, v):
+        covariance_function.__init__(self, lengthscale, v)
+
+    def evaluate(self, x_i, x_j):
+        return np.exp(-1*np.sqrt(np.dot((x_i-x_j).transpose(), (x_i-x_j))))
+
+class periodic1(covariance_function):
+
+    def __init__(self, lengthscale, v):
+        covariance_function.__init__(self, lengthscale, v)
+    
+    def evaluate(self, x_i, x_j):
+        return np.exp(-1*np.sin(5*np.pi*(x_i-x_j))**2)
+        
+class matern(covariance_function):
+
+    def __init__(self, lengthscale, v):
+        covariance_function.__init__(self, lengthscale, v)
+
+    def evaluate(self, x_i, x_j):
+        dist = np.linalg.norm(x_i-x_j)
+        return np.nan_to_num(((2**(1-self.v))/(ss.gamma(self.v))) * ((np.sqrt(2*self.v) * (dist/self.lengthscale))**self.v) * ss.kv(self.v, (np.sqrt(2*self.v) * (dist/self.lengthscale))))
